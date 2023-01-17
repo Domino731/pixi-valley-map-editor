e5c3faf46edc7665c268e0bcca78fcb4391ccab7
Domino731 <d.orzechowski18@gmail.com>
2023-01-17 17:45:06 +0100

    game data - oak, pine, mahogany, maple trees

--- src/data/environment/trees/index.ts
+++ src/data/environment/trees/index.ts
@@ -1,6 +1,17 @@
-import {MapleTree} from "./Maple";
 import {TreeObject} from "../types";
+import {treeFactory} from "./utils";
+import {TREE_NAMES} from "./const";
 
+// create trees using treeFactory function which is creating tree with stages (1-5)
+const Maple: TreeObject = treeFactory(TREE_NAMES.MAPLE);
+const Oak: TreeObject = treeFactory(TREE_NAMES.MAPLE);
+const Pine: TreeObject = treeFactory(TREE_NAMES.PINE);
+const Mahogany: TreeObject = treeFactory(TREE_NAMES.MAHOGANY);
+
+/** Array with all trees */
 export const Trees: Array<TreeObject> = [
-    MapleTree
+    Maple,
+    Oak,
+    Pine,
+    Mahogany
 ]
--- src/data/environment/trees/utils.ts
+++ src/data/environment/trees/utils.ts
@@ -3,9 +3,29 @@ import {TREE_ITEMS, TreeObject, TreeStageObject} from "../types";
 import {ENGINE_OBJECT_GROUPS, ENGINE_OBJECTS_TYPES, SPRITE_SRC} from "../../types";
 import {TOOLS} from "../../tools/types";
 
-const TREE_STAGES: number = 5;
+const TREE_STAGES: number = 4;
 
 export const treeFactory = (treeName: keyof typeof TREE_NAMES): TreeObject => {
+    let xOffset: number = 0;
+
+    switch (treeName) {
+        case TREE_NAMES.MAPLE:
+            xOffset = 0;
+            break;
+        case TREE_NAMES.OAK:
+            xOffset = 1;
+            break;
+        case TREE_NAMES.PINE:
+            xOffset = 2;
+            break;
+        case TREE_NAMES.MAHOGANY:
+            xOffset = 3;
+            break;
+        default:
+            break;
+    }
+
+
     const Tree: TreeObject = {
         group: ENGINE_OBJECT_GROUPS.ENVIROMENT,
         type: ENGINE_OBJECTS_TYPES.TREES,
@@ -27,18 +47,18 @@ export const treeFactory = (treeName: keyof typeof TREE_NAMES): TreeObject => {
         stages: []
     }
 
-    for (let i = 1; i <= 4; i++) {
+    for (let i = 1; i <= TREE_STAGES; i++) {
         const stageObject: TreeStageObject = {
             group: ENGINE_OBJECT_GROUPS.ENVIROMENT,
             type: ENGINE_OBJECTS_TYPES.TREES,
-            id: TREE_NAMES.MAPLE_SUMMER,
-            name: `${TREE_NAMES.MAPLE_SUMMER} stage ${i}`,
+            id: `${treeName} stage ${i}`,
+            name: `${treeName} stage ${i}`,
             hp: 800,
             sprite: {
                 // @ts-ignore
                 src: SPRITE_SRC[`TREE_STAGE_${i}`],
                 position: {
-                    x: 1,
+                    x: xOffset,
                     y: 0,
                 }
             },
