81d77c29247d8cad339fe39299c7b3ca6561cd5c
Domino731 <d.orzechowski18@gmail.com>
2023-01-25 17:13:58 +0100

    feature - inspect - panel buttons

--- src/app/LeftPanel/Inspect/index.ts
+++ src/app/LeftPanel/Inspect/index.ts
@@ -23,15 +23,25 @@ export class Inspect {
         return Object.values(GAME_DATA.objects).flat().find(({id}: { id: string }) => id === objectId);
     }
 
+    /**
+     * Add object image to blueprint
+     * @param objectId - object's id needed to find the specific one data
+     * */
     public static addImage(objectId: string): void {
+        // get data - object, sprite size, dom element
         const {sprite}: EngineObject = this.findEngineObjectById(objectId);
         const {cellWidth, cellHeight}: SpriteSize = this.findObjectSize(sprite.src);
         const blueprint: HTMLDivElement = document.querySelector('#inspect-blueprint');
+        const blueprintLabel: HTMLParagraphElement = document.querySelector('#inspect-blueprint-label');
 
+        // apply styles
         blueprint.style.width = `${cellWidth}px`;
         blueprint.style.height = `${cellHeight}px`;
         blueprint.style.backgroundImage = `url(./src/sprites/${sprite.src})`;
         blueprint.style.backgroundPosition = `-${sprite.position.x * cellWidth}px -${sprite.position.y * cellHeight}px`;
+
+        // hide label
+        blueprintLabel.classList.add('hide');
     }
 
 }
