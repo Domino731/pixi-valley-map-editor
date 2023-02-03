4d0c19e7d72d5948f5511eb0c4aab8713ecacac9
Domino731 <d.orzechowski18@gmail.com>
2023-02-03 15:54:43 +0100

    feature - inspect world - toggle overview panel

--- src/app/LeftPanel/ToggleDebuggerMode/index.ts
+++ src/app/LeftPanel/ToggleDebuggerMode/index.ts
@@ -7,7 +7,13 @@ export class ToggleDebuggerMode {
         objects: HTMLDivElement;
         world: HTMLDivElement;
     }
+    private readonly inspectOverview: {
+        objects: HTMLDivElement;
+        world: HTMLDivElement;
+    }
 
+
+    // inspect-object-overview
     constructor() {
         this.buttons = {
             objects: document.querySelector('#toggle-debugger-sections-button-objects'),
@@ -17,6 +23,10 @@ export class ToggleDebuggerMode {
             objects: document.querySelector('#inspect-objects-section'),
             world: document.querySelector('#inspect-world-section')
         }
+        this.inspectOverview = {
+            objects: document.querySelector('#inspect-object-overview'),
+            world: document.querySelector('#inspect-world-overview')
+        }
 
         this.init();
     }
@@ -24,11 +34,15 @@ export class ToggleDebuggerMode {
     private buttonEvents(): void {
         this.buttons.world.addEventListener('click', () => {
             this.inspectSections.world.classList.remove('hide');
+            this.inspectOverview.world.classList.remove('hide');
             this.inspectSections.objects.classList.add('hide');
+            this.inspectOverview.objects.classList.add('hide');
         });
         this.buttons.objects.addEventListener('click', () => {
             this.inspectSections.objects.classList.remove('hide');
+            this.inspectOverview.objects.classList.remove('hide');
             this.inspectSections.world.classList.add('hide');
+            this.inspectOverview.world.classList.add('hide');
         });
     }
 
