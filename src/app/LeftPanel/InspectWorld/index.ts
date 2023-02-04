d79f70b45a983fceb5a90c720f3cd95107c8163f
Domino731 <d.orzechowski18@gmail.com>
2023-02-04 14:56:09 +0100

    feature - inspect world - set active section

--- src/app/LeftPanel/InspectWorld/index.ts
+++ src/app/LeftPanel/InspectWorld/index.ts
@@ -1,11 +1,41 @@
-import {InspectWorldObjects} from "./components/InspectWorldObjects";
+import {EngineObjectTypesUnion} from "../../../data/types";
 
 export class InspectWorld {
+    private readonly buttonElements: Array<HTMLButtonElement>;
+    private readonly sectionElements: Array<HTMLDivElement>;
+
     constructor() {
+        this.buttonElements = document.querySelectorAll('#inspect-world-buttons button') as unknown as Array<HTMLButtonElement>;
+        this.sectionElements = document.querySelectorAll('#inspect-world-section div[data-section-panel-name]') as unknown as Array<HTMLDivElement>;
         this.init();
     }
 
+    private openSection(buttonName: EngineObjectTypesUnion): void {
+        this.sectionElements.forEach((sectionElement: HTMLDivElement) => {
+            if (buttonName === sectionElement.dataset.sectionPanelName) {
+                sectionElement.classList.remove('hide');
+            } else {
+                sectionElement.classList.add('hide');
+            }
+        })
+    }
+
+    private setAllButtonsDisabled(): void {
+        this.buttonElements.forEach((buttonElement: HTMLButtonElement) => buttonElement.className = 'inspect__panelButton inspect__panelButton--disabled');
+    }
+
+    private buttonClickEvents(): void {
+        this.buttonElements.forEach((buttonElement: HTMLButtonElement) => {
+            buttonElement.addEventListener('click', (e) => {
+                this.setAllButtonsDisabled();
+                buttonElement.className = 'inspect__panelButton inspect__panelButton--active'
+                this.openSection(buttonElement.dataset.sectionName as EngineObjectTypesUnion)
+            })
+        })
+    }
+
     private init(): void {
+        this.buttonClickEvents();
         // new InspectWorldObjects().build();
     }
 }
