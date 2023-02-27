3c70251ddf00bca0df901f2929053a69eee77cc9
Domino731 <d.orzechowski18@gmail.com>
2023-02-27 16:06:20 +0100

    auto flooring - const

---
+++ src/utils/flooringTextures.ts
@@ -0,0 +1,25 @@
+enum FLOORING_BORDERS {
+    TOP_RIGHT_BOT_LEFT = 'TOP_RIGHT_BOT_LEFT',
+    TOP_RIGHT_LEFT = 'TOP_RIGHT_LEFT',
+    RIGHT_LEFT = 'RIGHT_LEFT',
+    RIGHT_BOT_LEFT = 'RIGHT_BOT_LEFT',
+
+    TOP_LEFT = 'TOP_LEFT',
+    LEFT = 'LEFT',
+    BOT_LEFT = 'BOT_LEFT',
+
+    TOP = 'TOP',
+    NONE = 'NONE',
+    BOT = 'BOT',
+
+    TOP_RIGHT = 'TOP_RIGHT',
+    RIGHT = 'RIGHT',
+    RIGHT_BOT = 'RIGHT_BOT',
+}
+
+const FLOORING_VECTORS = {}
+
+
+export const flooringTextures = () => {
+
+}
