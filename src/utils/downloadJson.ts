35f67714e1da3934c867ba95671c6060089f4f76
Domino731 <d.orzechowski18@gmail.com>
2023-03-01 17:34:38 +0100

    general - downloadJson utility improvements

--- src/utils/downloadJson.ts
+++ src/utils/downloadJson.ts
@@ -4,10 +4,14 @@
 * @Param fileName - name of the file
 *  */
 export const downloadJson = (exportObject: Record<string, any>, fileName: string): void => {
+    // create data with passed export object
     const dataStr: string = "data:text/json;charset=utf-8," + encodeURIComponent(JSON.stringify(exportObject));
+    // create anchor needed for click event
     const downloadAnchorNode: HTMLAnchorElement = document.createElement('a');
+    // set appropriate attributes
     downloadAnchorNode.setAttribute("href", dataStr);
     downloadAnchorNode.setAttribute("download", fileName + ".json");
+    // simulate click on anchor to make file start downloading
     document.body.appendChild(downloadAnchorNode);  // required for firefox
     downloadAnchorNode.click();
     downloadAnchorNode.remove();
