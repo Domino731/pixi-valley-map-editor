a4a7f9ea73ac4c67451df853e1cec6e24de03df5
Domino731 <d.orzechowski18@gmail.com>
2023-03-01 17:36:43 +0100

    general - getSpriteSrc utility improvements

--- src/utils/getSpriteSrc.ts
+++ src/utils/getSpriteSrc.ts
@@ -1,4 +1,8 @@
+/**
+ * get sprite source path
+ * @Param backgroundImage - background image url
+ * */
 export const getSpriteSrc = (backgroundImage: string): string => {
-    const url = backgroundImage.slice((backgroundImage.lastIndexOf('/')) * -1);
+    const url: string = backgroundImage.slice((backgroundImage.lastIndexOf('/')) * -1);
     return url.slice(0, url.length - 2)
 }
