18261ad488615dac1cc0a9d13cd49290145f2e94
Domino731 <d.orzechowski18@gmail.com>
2023-03-01 17:40:17 +0100

    general - setPanelButton utility improvements

--- src/utils/setPanelButton.ts
+++ src/utils/setPanelButton.ts
@@ -1,12 +1,21 @@
-const MAIN_CLASS = 'inspect__panelButton'
-const DISABLED = `${MAIN_CLASS} ${MAIN_CLASS}--disabled`;
-const ACTIVE = `${MAIN_CLASS} ${MAIN_CLASS}--active`;
+const MAIN = 'inspect__panelButton'
+const DISABLED = `${MAIN} ${MAIN}--disabled`;
+const ACTIVE = `${MAIN} ${MAIN}--active`;
 
+// These functions are responsible for set appropriate styles for active & disabled "toggle" button
 
+/**
+ * Set panel button active by adding appropriate class name
+ * @Param button - button that you want to make active
+ * */
 export const setPanelButtonActive = (button: HTMLButtonElement): void => {
     button.className = ACTIVE;
 }
 
+/**
+ * Set panel button active by adding appropriate class name
+ * @Param button - button that you want to make disabled
+ * */
 export const setPanelButtonDisabled = (button: HTMLButtonElement): void => {
     button.className = DISABLED;
 }
