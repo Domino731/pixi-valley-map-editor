a4762dab9deec0215497151af9305e2c9d46cc2b
Domino731 <d.orzechowski18@gmail.com>
2023-03-07 18:55:21 +0100

    refactor - objectUtils

--- src/utils/objectUtils.ts
+++ src/utils/objectUtils.ts
@@ -1,10 +1,12 @@
 import {GAME_DATA} from "../data";
 import {EngineObject} from "../data/types";
 
-export const getObjectNameById = (objectId: string): string => {
+export const getObjectById = (objectId: string): EngineObject | null => {
     const allObjects: Array<EngineObject> = Object.values(GAME_DATA.objects).flat()
-    return allObjects.find(({id}) => id === objectId)?.name ?? '';
+    return allObjects.find(({id}) => id === objectId) ?? null;
 }
+
+export const getObjectNameById = (objectId: string): string => getObjectById(objectId)?.name ?? '';
 
 export const findObjectOnMap = (objectMapId: string): HTMLDivElement | null => {
     return document.getElementById(`${objectMapId}`) as HTMLDivElement | null;
