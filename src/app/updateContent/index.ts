5555f8ec05a1d7b0fe6d3f3773b7c16fb38e836c
Domino731 <d.orzechowski18@gmail.com>
2023-03-08 16:39:26 +0100

    feature - update content - general assumptions

--- src/app/updateContent/index.ts
+++ src/app/updateContent/index.ts
@@ -4,3 +4,12 @@ import {EngineObject} from "../../data/types";
 export const updateContent = (type: UPDATE_CONTENT_TYPE, object: EngineObject): void => {
     const allElements = [];
 }
+
+
+// 1. Stworzyć listę gdzie i kiedy powinien odbywać się update
+// 2. Dodać do każdego kontenera obiektu odpowiedni dataset attribute (użyć do tego funkcji setObjectElementAttribute())
+// 3. Pobierać wszystkie elementy
+// 4. stworzyć switch case aby łatwo zarządzać aktualizacją
+// 5. Optymalizacja
+// 6. Kalsa main refactor
+// 7. Testy
