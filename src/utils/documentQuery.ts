7c967da4b08ae364c3c83648654e9866f6c75f0c
Domino731 <d.orzechowski18@gmail.com>
2023-04-07 15:54:26 +0200

    new crops

--- src/utils/documentQuery.ts
+++ src/utils/documentQuery.ts
@@ -3,5 +3,5 @@ type UtilsReturn = HTMLElement | null
 const query = (selector: string): UtilsReturn => document.querySelector(selector);
 
 export const findEngineObjectOnMap = (objectMapId: string): UtilsReturn => {
-    return query(`#${objectMapId}`)
+    return query(`#${objectMapId}`);
 }
